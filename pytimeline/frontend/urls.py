f639a0668c637b4a0f70b5cb34b18fbe381830d4
godoy <godoy@pop-os.localdomain>
2023-03-24 23:58:34 -0300

    add: more files and urls and views

--- pytimeline/frontend/urls.py
+++ pytimeline/frontend/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import Home, CreatePlayRoom, PlayRoom, UserPlayView, JoinPlayRoom
 
 urlpatterns = [
-    path('board/', TemplateView.as_view(template_name="board.html")),
-
-    path('boardc/', TemplateView.as_view(template_name="board copy.html")),
+    path('board/', Home.as_view()),
+    path('board/create', CreatePlayRoom.as_view(), name="create-board"),
+    path('board/join', JoinPlayRoom.as_view()),
+    path('board/<slug:bord_slug>/', PlayRoom.as_view(), name="play-room"),
+    path('board/<slug:bord_slug>/new_user', JoinPlayRoom.as_view()),
+    path('board/<slug:bord_slug>/user/1', UserPlayView.as_view(), name="user-play-view"),
 
 ]
 

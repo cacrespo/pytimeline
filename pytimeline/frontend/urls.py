81a4b8586956c2032d454f5b45412b47fee034bb
godoy <godoy@pop-os.localdomain>
2023-03-25 12:52:53 -0300

    update views css

--- pytimeline/frontend/urls.py
+++ pytimeline/frontend/urls.py
@@ -5,8 +5,10 @@ from .views import Home, CreatePlayRoom, PlayRoom, UserPlayView, JoinPlayRoom
 urlpatterns = [
     path('board/', Home.as_view()),
     path('board/create', CreatePlayRoom.as_view(), name="create-board"),
-    path('board/join', JoinPlayRoom.as_view()),
-    path('board/<slug:bord_slug>/', PlayRoom.as_view(), name="play-room"),
+    path('board/join', JoinPlayRoom.as_view(), name="join-play-room"),
+    path('board/join/<int:pk>/', JoinPlayRoom.as_view(), name="join-play-room"),
+
+    path('board/<int:pk>/', PlayRoom.as_view(), name="play-room"),
     path('board/<slug:bord_slug>/new_user', JoinPlayRoom.as_view()),
     path('board/<slug:bord_slug>/user/1', UserPlayView.as_view(), name="user-play-view"),
 
--- pytimeline/frontend/views.py
+++ pytimeline/frontend/views.py
@@ -1,5 +1,8 @@
 # some_app/views.py
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+
+from engine.models import Game
 
 
 class Home(TemplateView):
@@ -9,10 +12,17 @@ class CreatePlayRoom(TemplateView):
     template_name = "create-play-room.html"
 
 class JoinPlayRoom(TemplateView):
-    template_name = "create-play-room.html"
+    template_name = "join-play-room.html"
 
-class PlayRoom(TemplateView):
+    def get_context_data(self, **kwargs):
+        kwargs = super(JoinPlayRoom, self).get_context_data(**kwargs)
+        return kwargs
+        
+
+
+class PlayRoom(DetailView):
     template_name = "play-room.html"
+    model = Game
 
 class UserPlayView(TemplateView):
     template_name = "user-play-view.html"

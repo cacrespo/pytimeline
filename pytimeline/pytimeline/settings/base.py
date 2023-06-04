66cbf10c1ff2314f839347b3d7963bf3b65bf909
Carlos A. Crespo <lvccrespo@gmail.com>
2023-06-04 00:08:20 -0300

    feat(settings): add multiple settings files

--- pytimeline/pytimeline/settings/base.py
+++ pytimeline/pytimeline/settings/base.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j+^*5e*dp&=7o=*5=5=xhcr9i!r99_xz25!)lwr08kk_9hu%o!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -66,7 +60,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': DEBUG,
         },
     },
 ]

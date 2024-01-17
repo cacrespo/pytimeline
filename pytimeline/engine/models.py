d3a212c84a8114fcb72e4514bb5a2724785ecccd
Carlos A. Crespo <lvccrespo@gmail.com>
2024-01-17 13:24:26 -0300

    tests!

--- pytimeline/engine/models.py
+++ pytimeline/engine/models.py
@@ -1,3 +1,4 @@
+import random
 from django.db import models
 
 
@@ -9,13 +10,32 @@ class Card(models.Model):
     text = models.CharField(max_length=500)
 
 class Game(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=256)
     deck = models.ManyToManyField("Card")
+
+    def deck_random_make(self, n_cards=10):
+        """
+        Randomly selects a specified number of cards and adds them to the
+        game's deck.
+
+        Parameters:
+        - n_cards (int): The number of cards to randomly select.
+        """
+        self.save()
+        self.deck.clear()
+
+        # Get a list of IDs of available cards
+        card_ids = list(Card.objects.values_list('pk', flat=True))
+
+        # Randomly select n_cards card IDs
+        selected_card_ids = random.sample(card_ids, min(n_cards, len(card_ids)))
+        selected_cards = Card.objects.filter(pk__in=selected_card_ids)
+        self.deck.add(*selected_cards)
+
 #    timeline = models.OneToOneField(
 #        Timeline,
 #        related_name="game",
 #        on_delete=models.CASCADE,
 #        null=True,
 #    )
-    def __str__(self):
-        return self.title
--- pytimeline/engine/tests.py
+++ pytimeline/engine/tests.py
@@ -3,11 +3,15 @@ from django.db.models import Q
 
 from .models import Game, Card
 
-def create_game(title, cards):
+def create_game(title, n_cards):
     """
     Create a game with the given random number of cards and a specific title.
     """
     game = Game(title=title)
+    game.deck_random_make(n_cards=20)
+
+    return game
+
 
 
 class CardModelTests(TestCase):
@@ -19,5 +23,12 @@ class CardModelTests(TestCase):
         cards = Card.objects.filter(Q(text="") | Q(date__isnull=True))
         self.assertEqual(cards.count(), 0)
 
+class GameModelTests(TestCase):
+    def test_game_different_deck_size_or_title(self):
+        """
+        General game with different deck size or title.
+        """
 
-# Check Game without deck
+        test_game = create_game("Test Game", n_cards=20)
+        self.assertEqual(test_game.deck.count(), 20)
+        self.assertEqual(test_game.title, "Test Game")

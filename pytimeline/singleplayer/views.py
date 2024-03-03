87c41a43246b4bb6e5996430a209342007578f4f
Carlos A. Crespo <lvccrespo@gmail.com>
2024-03-03 17:38:27 -0300

    feat(singleplayer): exclusive play view

--- pytimeline/singleplayer/views.py
+++ pytimeline/singleplayer/views.py
@@ -4,6 +4,9 @@ from . import urls
 from singleplayer.models import SinglePlayerGame
 
 def index(request):
+   return render(request, "singleplayer/singleplayer.html")
+
+def play(request):
     # Check if the game instance is already stored in the session
     if 'game_id' in request.session:
         game_id = request.session['game_id']
@@ -16,4 +19,6 @@ def index(request):
         # Store the game instance ID in the session
         request.session['game_id'] = game.pk
 
-    return render(request, "singleplayer/singleplayer.html", {'game': game})
+    return render(request, "singleplayer/singleplayer_play.html", {'game': game})
+
+

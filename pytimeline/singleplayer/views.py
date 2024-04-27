f6275ada3eed2d7e56724fbde40f7be57934e15f
Carlos A. Crespo <lvccrespo@gmail.com>
2024-04-27 15:19:47 -0300

    fix(singleplayer): fix the displayed cards order on the end game

--- pytimeline/singleplayer/views.py
+++ pytimeline/singleplayer/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 from . import urls
 from singleplayer.models import SinglePlayerGame
 from engine.models import Card
+from django.db.models import Case, When
+
 
 def index(request):
-   return render(request, "singleplayer/singleplayer.html")
+    return render(request, "singleplayer/singleplayer.html")
+
 
 def play(request):
     # Check if the game instance is already stored in the session
@@ -22,11 +25,14 @@ def play(request):
 
     return render(request, "singleplayer/singleplayer_play.html", {'game': game})
 
+
 def end(request):
     if request.method == 'POST':
         answers = request.POST.get('answer').split(",")
         answers = list(map(int, answers))
-        cards = Card.objects.filter(id__in=answers)
+        _order = Case(*(When(id=id, then=pos)
+                      for pos, id in enumerate(answers, start=1)))
+        cards = Card.objects.filter(id__in=answers).order_by(_order)
         ordered_answers = [card.id for card in cards.order_by('date')]
         results = zip(cards, ordered_answers)
-    return render(request, "singleplayer/singleplayer_end.html", {'results': results })
+    return render(request, "singleplayer/singleplayer_end.html", {'results': results})
